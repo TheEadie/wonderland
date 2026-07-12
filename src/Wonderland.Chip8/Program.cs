6d37aad0f06cb69c23736bace5ac479a9d93832a
David Eadie <david@eadie.net>
2026-07-12 21:15:11 +0100

    Upgrade to net10.0 and System.CommandLine 2.0.9, keep SFML.NET pinned at 2.6.1
    SFML.NET 3.0.0's bundled Linux native libs require glibc 2.38, which isn't
    available on this machine's Debian 12 (glibc 2.36) yet. Stay on the 2.x API
    until glibc support catches up across machines.

--- src/Wonderland.Chip8/Program.cs
+++ src/Wonderland.Chip8/Program.cs
@@ -1,23 +1,24 @@
 using System.CommandLine;
 using Wonderland.Chip8;
 
-var romArg = new Argument<string>(
-    name: "rom-path",
-    description: "The path to the ROM to use in the emulator."
-);
+var romArg = new Argument<string>("rom-path")
+{
+    Description = "The path to the ROM to use in the emulator."
+};
 
-var clockSpeedOption = new Option<int>(
-    name: "--clock-speed",
-    description: "The target clock speed to be used in Hz.",
-    getDefaultValue: () => 1000);
+var clockSpeedOption = new Option<int>("--clock-speed")
+{
+    Description = "The target clock speed to be used in Hz.",
+    DefaultValueFactory = _ => 1000
+};
 
 var rootCommand = new RootCommand("Wonderland Chip-8 emulator");
-rootCommand.AddArgument(romArg);
-rootCommand.AddOption(clockSpeedOption);
+rootCommand.Arguments.Add(romArg);
+rootCommand.Options.Add(clockSpeedOption);
 
-rootCommand.SetHandler(RunEmulator, romArg, clockSpeedOption);
+rootCommand.SetAction(parseResult => RunEmulator(parseResult.GetValue(romArg)!, parseResult.GetValue(clockSpeedOption)));
 
-return await rootCommand.InvokeAsync(args);
+return await rootCommand.Parse(args).InvokeAsync();
 
 
 void RunEmulator(string romPath, int clockSpeed)
--- src/Wonderland.GameBoy/Program.cs
+++ src/Wonderland.GameBoy/Program.cs
@@ -1,17 +1,17 @@
 ﻿using System.CommandLine;
 using Wonderland.GameBoy;
 
-var romArg = new Argument<string>(
-    name: "rom-path",
-    description: "The path to the ROM to use in the emulator."
-);
+var romArg = new Argument<string>("rom-path")
+{
+    Description = "The path to the ROM to use in the emulator."
+};
 
 var rootCommand = new RootCommand("Wonderland GameBoy emulator");
-rootCommand.AddArgument(romArg);
+rootCommand.Arguments.Add(romArg);
 
-rootCommand.SetHandler(RunEmulator, romArg);
+rootCommand.SetAction(parseResult => RunEmulator(parseResult.GetValue(romArg)!));
 
-return await rootCommand.InvokeAsync(args);
+return await rootCommand.Parse(args).InvokeAsync();
 
 
 void RunEmulator(string romPath)
