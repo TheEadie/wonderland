181967f4dd53454f87fd5690528e6e6c8139cf88
David Eadie <david@eadie.net>
2026-07-13 10:58:21 +0100

    Add remaining unconditional/conditional JR opcodes (0x18, 0x28, 0x38)

---
+++ src/Wonderland.GameBoy/OpCodes/JumpAndCalls/JR_Z_s8.cs
@@ -0,0 +1,25 @@
+namespace Wonderland.GameBoy.OpCodes.JumpAndCalls;
+
+public record JR_Z_s8() : OpCode(
+    0x28,
+    "JR Z s8",
+    2,
+    12,
+    [
+        (r, m, _) =>
+            {
+                r.SubOp_SignedByte = m.GetSignedMemory(r.PC++);
+                return false;
+            },
+        (r, _, _) =>
+            {
+                if (!r.FlagZ)
+                {
+                    return true;
+                }
+
+                r.PC = (ushort)(r.PC + r.SubOp_SignedByte);
+                return false;
+            },
+        (_, _, _) => true
+    ]);
