3888cb5e914cb91ff12144fda06ec53ef54aabfa
David Eadie <david@eadie.net>
2026-07-13 10:58:17 +0100

    Add remaining conditional/unconditional JP opcodes (0xC2, 0xCA, 0xD2, 0xDA, 0xE9)

---
+++ src/Wonderland.GameBoy/OpCodes/JumpAndCalls/JP_Z_u16.cs
@@ -0,0 +1,30 @@
+namespace Wonderland.GameBoy.OpCodes.JumpAndCalls;
+
+public record JP_Z_u16() : OpCode(
+    0xCA,
+    "JP Z u16",
+    3,
+    16,
+    [
+        (r, m, _) =>
+            {
+                r.SubOp_LSB = m.GetMemory(r.PC++);
+                return false;
+            },
+        (r, m, _) =>
+            {
+                r.SubOp_MSB = m.GetMemory(r.PC++);
+                return false;
+            },
+        (r, _, _) =>
+            {
+                if (!r.FlagZ)
+                {
+                    return true;
+                }
+
+                r.PC = Bits.CreateU16(r.SubOp_MSB, r.SubOp_LSB);
+                return false;
+            },
+        (_, _, _) => true
+    ]);
